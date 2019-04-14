0cf077de03c78e3f761b63592467afe7fa690f99
alligatorYE <alligator_y@qq.com>
2019-04-14 18:09:21 +0800

    Initial commit

---
+++ src/com/alligator/Main.java
@@ -0,0 +1,22 @@
+package com.alligator;
+
+public class Main {
+public static final int num1 = 1314;
+
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String sayILoveU = "I love you !";
+        System.out.println("hello world ！" + sayILoveU);
+
+        System.out.println("你好 世界！" + sayILoveU);
+        System.out.println("你好 世界！" + sayILoveU);
+        System.out.println("你好 世界！" + sayILoveU);
+        System.out.println("你好 世界！" + sayILoveU);
+        System.out.println("你好 世界！" + sayILoveU);
+        System.out.println("你好 世界！" + sayILoveU);
+        System.out.println(num1);
+    }
+}
+
