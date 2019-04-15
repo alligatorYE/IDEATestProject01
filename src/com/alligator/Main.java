c4d3a38a534e14d69ad208b1f5b0a7eb3b1588a2
alligatorYE <alligator_y@qq.com>
2019-04-15 12:11:51 +0800

    配置自动化构建工具

--- src/com/alligator/Main.java
+++ src/com/alligator/Main.java
@@ -1,22 +1,20 @@
 package com.alligator;
 
 public class Main {
-public static final int num1 = 1314;
+    public static final int num1 = 1314;
 
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+//    HashMapTest hashMapTest = new HashMapTest();
 
-        String sayILoveU = "I love you !";
-        System.out.println("hello world ！" + sayILoveU);
-
-        System.out.println("你好 世界！" + sayILoveU);
-        System.out.println("你好 世界！" + sayILoveU);
-        System.out.println("你好 世界！" + sayILoveU);
-        System.out.println("你好 世界！" + sayILoveU);
-        System.out.println("你好 世界！" + sayILoveU);
-        System.out.println("你好 世界！" + sayILoveU);
-        System.out.println(num1);
+        int i = 1;
+        i = i++;
+        int j = i++;
+        int k = i+ ++i*i++;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
     }
 }
 
